fe26cb7991ec5c9a8a573d789ad5d4ab6ff3afd6
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-02-01 14:13:09 -0400

    Wired up User action, reducer & rendered userId to UI

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -3,5 +3,11 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPosts = () => async (dispatch:any) => {
         const res = await jsonPlaceholder.get('/posts');
 
-        dispatch({ type: 'FETCH_POSTS', payload: res.data })
+        dispatch({ type: 'FETCH_POSTS', payload: res.data });
     };
+
+export const fetchUser = (id:any) => async (dispatch:any) => {
+    const res = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: res.data });
+};
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component<any> {
     componentDidMount() {
         this.props.fetchPosts();
     };
 
-    render() {
-        console.log(this.props.posts);
+    renderList() {
+        return this.props.posts.map((post:any) => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user"/>
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                        <UserHeader userId={post.userId}/>
+                    </div>
+                </div>
+            );
+        });
+    }
 
+    render() {
         return (
-            <div>PostList</div>
+            <div className="ui relaxed divided list">{this.renderList()}</div>
         );
     };
 };
